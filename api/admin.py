cb0956a9a23768ee1c4799d23b209015b6a2ebe8
risen_73 <blokhina.a.u@gmail.com>
2024-11-06 01:00:21 +0300

    feat: add question and content model

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Question, Content
 
-# Register your models here.
+class ContentInline(admin.TabularInline):
+    model = Content
+    extra = 1
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ContentInline]
+
--- api/models.py
+++ api/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    CATEGORIES_CHOICES = (
+        (None, 'Вопрос default'),
+        ('blitz', 'Блиц'),
+        ('superblitz', 'Суперблиц'),
+        ('blackbox', 'Чёрный ящик'),
+        ('handout', 'Вопрос с раздаткой'),
+        ('warm-up', 'Нулевой вопрос'),
+        ('extra', 'Запасной вопрос'),
+        ('bonus', 'Бонусный вопрос'),
+    )
+    
+    seen = models.BooleanField(default=False, verbose_name='Сыгран')
+    category = models.CharField(
+        null=True,
+        blank=True,
+        max_length=20,
+        choices=CATEGORIES_CHOICES,
+        verbose_name='Категория'
+    )
+
+    def __str__(self):
+        if self.seen:
+            return f'{self.id} СКРЫТ'
+        else:
+            return f'{self.id}'
+
+
+class Content(models.Model):
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE,
+        related_name='content',
+        verbose_name='Текст вопроса'
+    )
+    body = models.TextField(null=True, blank=True, verbose_name='Текст вопроса')
+
+    def __str__(self):
+        return f'Текст {self.question.id} вопроса'
