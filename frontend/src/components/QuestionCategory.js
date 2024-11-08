669faf3ce85f6a2961fbf64432fc4e661262a27e
risen_73 <blokhina.a.u@gmail.com>
2024-11-08 20:18:52 +0300

    feat: add category page music

--- frontend/src/components/QuestionCategory.js
+++ frontend/src/components/QuestionCategory.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import blackboxIcon from '../assets/images/blackbox.png';
 import handoutIcon from '../assets/images/handout.png';
 import blitzIcon from '../assets/images/blitz.png';
 import superblitzIcon from '../assets/images/superblitz.png';
+import clapMusic from '../assets/music/clap.mp3';
 
 const QuestionCategory = ( { category, onShowContent }) => {
   const transcription = {
@@ -19,11 +20,24 @@ const QuestionCategory = ( { category, onShowContent }) => {
     "superblitz" : superblitzIcon,
   }
 
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    if (category === "blackbox" || category === "handout") {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  }, [category]);
+
   return (
     <div className="category">
       <h2 className="category__title">{transcription[category]}</h2>
       <img className="category__image" src={image[category]} id={category} alt="" />
       <button className="category__next" onClick={onShowContent}>Перейти к вопросу</button>
+      <audio ref={audioRef} loop>
+        <source src={clapMusic} type="audio/mpeg"/>
+      </audio>
     </div>
   )
 }
