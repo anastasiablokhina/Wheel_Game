165cd272e867257d2a47166bec3e3b35dbc3eae1
risen_73 <blokhina.a.u@gmail.com>
2024-11-08 20:31:44 +0300

    feat: add timer music

--- frontend/src/components/Timer.js
+++ frontend/src/components/Timer.js
@@ -1,13 +1,20 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Toggle from './Toggle';
+import startMusic from '../assets/music/start.mp3';
+import middleMusic from '../assets/music/middle.mp3';
+import endMusic from '../assets/music/end.mp3';
 
 const Timer = ( { initialTime = 60, blitzIsFinished = false, setBlitzFinished = false }) => {
   const [isRunning, setRunning] = useState(false);
   const [isFinished, setFinished] = useState(false);
   const [isPaused, setPaused] = useState(false);
   const [secondsRemaining, setSecondsRemaining] = useState(initialTime);
+  const audioRefStart = useRef(null);
+  const audioRefMiddle = useRef(null);
+  const audioRefEnd = useRef(null);
 
   const startTimer = () => {
+    audioRefStart.current.play()
     setRunning(true);
     setFinished(false);
   };
@@ -49,6 +56,15 @@ useEffect(() => {
   }
 }, [blitzIsFinished, setBlitzFinished, initialTime])
 
+useEffect(() => {
+  if (secondsRemaining === 10 && initialTime === 60) { 
+    audioRefMiddle.current.play();
+  } else if (secondsRemaining === 0) {
+    audioRefEnd.current.play();
+    setFinished(true);
+  }
+}, [secondsRemaining, initialTime]);
+
   return (
     <div className="timer">
       {!isRunning ? (
@@ -62,6 +78,15 @@ useEffect(() => {
         </div>
         )
       }
+      <audio ref={audioRefStart}>
+        <source src={startMusic} type="audio/mpeg" />
+      </audio>
+      <audio ref={audioRefMiddle}>
+        <source src={middleMusic} type="audio/mpeg" />
+      </audio>
+      <audio ref={audioRefEnd}>
+        <source src={endMusic} type="audio/mpeg" />
+      </audio>
     </div>
   )
 }
