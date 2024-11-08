3dba726240a402ece22d189ed70c4ccc178f4fa7
risen_73 <blokhina.a.u@gmail.com>
2024-11-08 14:55:08 +0300

    feat: make question appear by click

--- frontend/src/components/QuestionContent.js
+++ frontend/src/components/QuestionContent.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 const QuestionContent = ( { questionId, question } ) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isContentVisible, setIsContentVisible] = useState(false);
   
   if (!question || !question.content || question.content.length === 0) {
     return null;
@@ -9,15 +10,23 @@ const QuestionContent = ( { questionId, question } ) => {
 
   const currentContent = question.content[currentIndex];
 
+  const handleScreenClick = () => {
+    if (!isContentVisible) {
+      setIsContentVisible(true);
+   }
+   };
+
   return (
-    <div className="content__wrapper">
+    <div className="content__wrapper" onClick={handleScreenClick}>
       <span></span>
       <span></span>
       <span></span>
       <span></span>
       <div className="content">
         <h1 className="content__title">Вопрос #{questionId}</h1>
-        <p className="content__text">{currentContent.body}</p>
+        {isContentVisible && (
+          <p className="content__text">{currentContent.body}</p>
+        )}
       </div>
     </div>
   )
